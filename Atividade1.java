1ddb38c64a121587cdc9b193d9de5556f1aeb690
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-10 14:11:40 -0300

    Atividade 2 feita

--- Atividade1.java
+++ Atividade1.java
@@ -1,8 +1,16 @@
 
+import java.util.Scanner;
+
 public class Atividade1 {
 
 	public static void main(String[] args) {
-		System.out.println("Olá Mundo");
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("Diga um número");
+	float num = input.nextFloat();
+	System.out.println("O número informado foi " + num);
+	input.close();
+		
 
 	}
 
