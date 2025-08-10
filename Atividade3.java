e1909ace889f840cb534a57b352016e08925ff8d
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-10 14:27:04 -0300

    Atividade 3 feita

--- Atividade3.java
+++ Atividade3.java
@@ -1,8 +1,17 @@
-
+import java.util.Scanner;
 public class Atividade3 {
 
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 		
+		System.out.println("Digite o primeiro número ");
+		float num1 = input.nextFloat();
+		System.out.println("Digite o segundo número ");
+		float num2 = input.nextFloat();
+		
+		input.close();
+		
+		System.out.println("Valor da soma " + (num1 + num2));
 
 	}
 
