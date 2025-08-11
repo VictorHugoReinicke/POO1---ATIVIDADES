9bc43c5352e9d699bcbfde9d49f7ef2086e93b7d
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-11 11:09:51 -0300

    Atividade 20 feita

---
+++ Atividade20.java
@@ -0,0 +1,21 @@
+import java.time.Year;
+import java.util.Scanner;
+public class Atividade20 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Ano");
+		int ano = input.nextInt();
+		String bissexto = "não é bissexto";
+		
+		if(Year.of(ano).isLeap()) 
+			bissexto = "é bissexto";
+		
+		
+		input.close();
+		System.out.println("O ano "+ bissexto);
+		
+	}
+
+}
