d975c7902300b078eab5794f022dfa5ef2a593f9
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-17 18:38:52 -0300

    Atividade 25 feita

---
+++ Atividade25.java
@@ -0,0 +1,15 @@
+
+public class Atividade25 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Lojas Quase Dois - Tabela de preços");
+		for(int i =1; i<= 50;i++) {
+			
+			System.out.println(i + " - R$ "+ (i*1.99));
+			
+		}
+		
+	}
+
+}
