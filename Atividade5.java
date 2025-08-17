18f4367222175dcebf14674244ab11fe4c70d877
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-17 15:30:21 -0300

    Atividade 5 feita

---
+++ Atividade5.java
@@ -0,0 +1,19 @@
+
+public class Atividade5 {
+
+	public static void main(String[] args) {
+	
+		for(int i =1; i <= 20; i++) {
+			
+			System.out.println(i);
+		
+		}
+		
+		for(int i =1; i <= 20; i++) {
+			
+			System.out.print(" "+i);
+		
+		}
+	}
+
+}
