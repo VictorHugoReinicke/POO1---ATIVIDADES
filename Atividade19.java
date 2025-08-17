4b029066e8a039d68185bbe3dcc7462834cf0696
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-17 17:26:16 -0300

    Atividade 19 feita

---
+++ Atividade19.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Atividade19 {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Digite o número");
+		int num = input.nextInt();
+		
+		int unNum =0;
+		
+		
+		for(int i =1; i <= num; i++) {
+			System.out.println("Divisão num "+i);
+			if((num%i) == 0)
+				unNum++;
+		}		
+		
+		input.close();
+		if(unNum ==2)
+			System.out.println("Primo");
+		
+
+	}
+
+}
