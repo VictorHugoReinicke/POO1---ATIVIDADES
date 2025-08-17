34f566b37296edbe5308afffa813181c5f13748d
Victor Hugo Reinicke <pombewcraft@gmail.com>
2025-08-17 16:47:02 -0300

    Atividade 15 feita

---
+++ Atividade15.java
@@ -0,0 +1,20 @@
+
+public class Atividade15 {
+
+	public static void main(String[] args) {
+		
+		int p = 37;
+		int s = 38;
+		float calc = 0;
+		for(int i=1;i <=37;i++) {
+			calc = (p*s)/i;
+			System.out.println("("+p+"*"+s+")/"+i);
+			System.out.println(calc);
+			p--;
+			s--;
+			
+		}
+
+	}
+
+}
